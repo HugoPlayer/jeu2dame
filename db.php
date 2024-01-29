e0d8c4aab87828ca46d19a35f387c89c3d4778d7
Hugo A <82807508+hugoallaine@users.noreply.github.com>
2024-01-29 11:49:35 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:host=localhost;dbname=id18636344_jeu2dame', 'id18636344_webmaster', 'sxXp4ZD#C)]N!YZY');
+    $db = new PDO('mysql:host=localhost;dbname='[DB NAME]', '[USERNAME DB]', '[PASSWORD]');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Echec de connexion : ".$e->getMessage();
